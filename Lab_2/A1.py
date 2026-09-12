f8953704792b3f8a3e6b6ab0ec385034d4be737b
Сергей Якимович <serginur97@gmail.com>
2026-09-12 16:07:09 +0300

    Исправлено

--- Lab_2/A1.py
+++ Lab_2/A1.py
@@ -6,7 +6,7 @@ b = width/2.0
 c = height/3
 d = 1 + 2 * 5
 
-print("width//2 имеет тип " + type(a) + " и значение " + a)
-print("width/2.0 имеет тип " + type(b) + " и значение " + b)
-print("height/3 имеет тип " + type(c) + " и значение " + c)
-print("1 + 2 * 5 имеет тип " + type(d) + " и значение " + d)
+print("width//2 имеет тип " + str(type(a)) + " и значение " + str(a))
+print("width/2.0 имеет тип " + str(type(b)) + " и значение " + str(b))
+print("height/3 имеет тип " + str(type(c)) + " и значение " + str(c))
+print("1 + 2 * 5 имеет тип " + str(type(d)) + " и значение " + str(d))
