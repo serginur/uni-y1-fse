974d8f50170a2164ae5adb9332c2cbe67ca47363
Сергей Якимович <serginur97@gmail.com>
2026-09-12 16:35:49 +0300

    Предполагаемое решение A2 и редактирование README.md

---
+++ Lab_2/A2.py
@@ -0,0 +1,8 @@
+print(int('123е'))
+print(int('91.4'))
+print(int(524.345 ** 435345345311145345))
+print(int('7.1 + 4'))
+print(int('4' - 2))
+print(int('4 - 2'))
+print(int('42'))
+print(int(-12.12))
