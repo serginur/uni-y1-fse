34ae014a039b21ba093ea38f44dd1564382534b5
Сергей Якимович <serginur97@gmail.com>
2026-09-12 16:05:00 +0300

    Предпологаемое решение для A1

---
+++ Lab_2/A1.py
@@ -0,0 +1,12 @@
+width = 17
+height = 12.0
+
+a = width//2
+b = width/2.0
+c = height/3
+d = 1 + 2 * 5
+
+print("width//2 имеет тип " + type(a) + " и значение " + a)
+print("width/2.0 имеет тип " + type(b) + " и значение " + b)
+print("height/3 имеет тип " + type(c) + " и значение " + c)
+print("1 + 2 * 5 имеет тип " + type(d) + " и значение " + d)
