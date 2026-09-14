75b66e2de83ea96605538aa229657c2a9ed29cf1
Сергей Якимович <serginur97@gmail.com>
2026-09-14 21:13:53 +0300

    Алгебра приведена в норму благодаря Decimal и ROUND_HALF_UP. Немного исправлено форматирование выводимого результата

--- Lab_2/b1.py
+++ Lab_2/b1.py
@@ -1,5 +1,6 @@
 from sys import argv
 import os
+from decimal import Decimal, ROUND_HALF_UP
 
 file_path = "b1_task_files/inmap0.dat"
 if (len(argv) > 1):
@@ -17,22 +18,25 @@ except PermissionError:
     exit(0)
 
 number_of_locations = int(lines[0].split()[0])
-map_scale = float(lines[0].split()[1])
+map_scale = Decimal(lines[0].split()[1]).quantize(Decimal("0.01"), ROUND_HALF_UP)
 distance_inches = []
 for i in range(1, len(lines)):
     if (lines[i]):
-        distance_inches.append(float(lines[i]))
+        distance_inches.append(Decimal(lines[i]).quantize(Decimal("0.1"), ROUND_HALF_UP))
 
 print("Sergey Iakimovich")
 print("Simple Map Distance Computations\n")
-print(f"Map Scale Factor:\t{map_scale:.2f} miles per inch\n")
+print(f"Map Scale Factor:\t{map_scale} miles per inch\n")
 
-print("      Map       Mileage")
-print("      Measure   Distance")
+print("      Map\tMileage")
+print("      Measure\tDistance")
 print("="*80)
-total = 0.0
+total = Decimal('0.0')
 for i in range(number_of_locations):
-    print(f"#  {i+1}    {distance_inches[i]:.1f}       {distance_inches[i] * map_scale:.1f}")
-    total += round(distance_inches[i] * map_scale, 1)
+    distance_inches_i = distance_inches[i]
+    distance_miles_i = Decimal(distance_inches[i] * map_scale).quantize(Decimal('0.1'), ROUND_HALF_UP)
+    spaces_to_print = 7 - len(str(distance_inches_i))
+    print(f"#  {i+1}{' '*spaces_to_print}{distance_inches_i}\t  {distance_miles_i}")
+    total += distance_miles_i
 print("="*80)
-print(f"Total distance:    {round(total,1)} miles")
+print(f"Total distance:    {total} miles")
