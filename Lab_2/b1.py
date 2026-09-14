fcb071fb04cf879122b27cfe922438eb8575709e
Сергей Якимович <serginur97@gmail.com>
2026-09-14 20:11:09 +0300

    Добавлена работа с аргументами командной строки: возможен ввод пути файла, из которого будет считываться информация для работы, в командной строке первым аргументом после имени файла скрипта; добавлена обработка возможных ошибок, связанных с отсутствием файла по введенному пути или недостаточностью прав для прочтения файла

--- Lab_2/b1.py
+++ Lab_2/b1.py
@@ -1,4 +1,21 @@
+from sys import argv
+import os
+
+file_path = "b1_task_files/inmap0.dat"
+if (len(argv) > 1):
+    file_path = argv[1]
+
+content = None
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+except FileNotFoundError:
+    print("Файл или директория отсутсвует!")
+    exit(0)
+except PermissionError:
+    print("Недостаточно прав доступа!")
+    exit(0)
+
 print("Sergey Iakimovich")
 print("Simple Map Distance Computations\n")
-
 print("Map Scale Factor:\t")
