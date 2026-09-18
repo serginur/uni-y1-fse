baec5926a1fc53c95088fdc089923f8957415b2d
Сергей Якимович <serginur97@gmail.com>
2026-09-18 21:08:07 +0300

    Добавлен алгоритм вычисления и вывод средней скорректированной температуры

--- Lab_2/b2.py
+++ Lab_2/b2.py
@@ -29,10 +29,21 @@ for i in range(2, len(lines)):
 print(f"Time{"WC temp":>12}{"WC Effect":>14}")
 print("-"*30)
 
+WC_temps = []
 i = 0
 while (i < len(time)):
     Twc = D(D("35.74")+D("0.6125")*D(air_temp[i]) + (D("0.4275")*D(air_temp[i]) - D("35.75"))*D(wind_speed[i])**D("0.16")).quantize(D("0.1"))
+    WC_temps.append(Twc)
     WC_effect = D(Twc - D(air_temp[i])).quantize(D("0.1"))
     print(f"{time[i]}{Twc:>8}{WC_effect:>14}")
     i += 1
 print("-"*30)
+
+i = 0
+print(f"The average adjusted tempterature, based on {len(time)} observations, was ", end='')
+avg_temp = D("0.0")
+while (i < len(WC_temps)):
+    avg_temp = avg_temp + WC_temps[i]
+    i += 1
+
+print(D(avg_temp/len(WC_temps)).quantize(D("0.1")))
