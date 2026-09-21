63f38d790c5d238ba41dfc4e8c73f8d789f40231
Сергей Якимович <serginur97@gmail.com>
2026-09-21 12:10:11 +0300

    Удален лишний import; добавлено хранение пройденной дистанции в словаре мыши и кота; switch заменен на if; добавлен алгоритм прибавки пройденной дистанции; добавлен триггер поимки мыши; добавлено вилка вывода конечных позиций игроков

--- Lab_3/b1.py
+++ Lab_3/b1.py
@@ -4,7 +4,6 @@
 также исход игры – «Кот поймал мышь» или «Мышь сбежала от кота» """
 
 from sys import argv
-from decimal import Decimal, ROUND_HALF_UP
 
 file_path = "b1_task_files/1.ChaseData.txt"
 if (len(argv) > 1):
@@ -34,31 +33,47 @@ print("Cat and Mouse\n")
 print(f"{"Cat":^7}{" "*4}{"Mouse":^7}{" "*4}{"Distance":^8}")
 print("-"*(7+4+7+4+8))
 
-C = {"lin": "?", "col": "?"}
-M = {"lin": "?", "col": "?"}
-for command in commands:
-    match command[0]:
-        case "P":
-            print(f"({C['lin']:>2},{C['col']:>2}){" "*4}", end='')
-            print(f"({M['lin']:>2},{M['col']:>2}){" "*4}", end='')
-            if (C["lin"] == "?" or M["lin"] == "?"):
-                print()
-            else:
-                print(f"{abs(C["lin"] - M["lin"])+abs(C["col"] - M["col"]):^8}")
-        case "C" | "M":
-            player = M if command[0] == 'M' else C
-            if (player["lin"] == "?"):
-                player["lin"] = int(command[1])
-                player["col"] = int(command[2])
-            else:
-                player["lin"] += int(command[1])
-                if (player["lin"] < 1):
-                    player["lin"] += lines_num
-                if (player["lin"] > lines_num):
-                    player["lin"] -= lines_num
-                player["col"] += int(command[2])
-                if (player["col"] < 1):
-                    player["col"] += columns_num
-                if (player["col"] > columns_num):
-                    player["col"] -= columns_num
+C = {"row": "?", "col": "?", "dis": 0}
+M = {"row": "?", "col": "?", "dis": -1}
+is_caught = False
 
+for command in commands:
+    if (command[0] == "P"):
+        print(f"({C["row"]:>2},{C["col"]:>2}){" "*4}", end='')
+        print(f"({M["row"]:>2},{M["col"]:>2}){" "*4}", end='')
+        if (C["row"] == "?" or M["row"] == "?"):
+            print()
+        else:
+            print(f"{(abs(C["row"] - M["row"])+abs(C["col"] - M["col"])):>8}")
+    elif (command[0] == "C" or command[0] == "M"):
+        player = M if command[0] == 'M' else C
+        if (player["row"] == "?"):
+            player["row"] = int(command[1])
+            player["col"] = int(command[2])
+        else:
+            player["row"] += int(command[1])
+            if (player["row"] < 1):
+                player["row"] += lines_num
+            if (player["row"] > lines_num):
+                player["row"] -= lines_num
+            player["col"] += int(command[2])
+            if (player["col"] < 1):
+                player["col"] += columns_num
+            if (player["col"] > columns_num):
+                player["col"] -= columns_num
+        if (player["dis"] == -1):
+            player["dis"] = 0 
+        else:
+            player["dis"] += abs(int(command[1])) + abs(int(command[2]))
+        if (M["col"] == C["col"] and M["row"] == C["row"]):
+            is_caught = True
+            break
+
+print("-"*(7+4+7+4+8)+'\n\n')
+print("Distance   Mouse    Cat")
+print(f"{' '*11}{M["dis"]:>5}{C["dis"]:>7}\n")
+
+if (is_caught):
+    print(f"Mouse caught at: ({M["row"]},{M["col"]})")
+else:
+    print("Mouse evaded cat")
