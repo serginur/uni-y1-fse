516d19adfe4908b2c944a87d6bf8fb58325aa878
Сергей Якимович <serginur97@gmail.com>
2026-09-26 09:33:10 +0300

    Изменена логика - теперь возможно бесконечное количество задаваемых примеров или ввод количества примеров при запуске программы в случае правильного использования аргументов командной строки. В соответствии с этим изменением изменилась и логика работы программы - отлавливаются прерывания с клавиатуры, добавлена обработка аргументов командной строки, изменен цикл, добавлена итерационная переменная, изменен вывод статистики

--- Lab_4/a1.py
+++ Lab_4/a1.py
@@ -1,24 +1,49 @@
 from random import randint
 import time
+from sys import argv
 
-n = int(input("Введите количество примеров: "))
+n = None
+if (len(argv) > 1):
+    if (argv[1] != "inf"):
+        try:
+            n = int(argv[1])
+        except ValueError:
+            print(f"usage: python {argv[0]} [num] ")
+            print("where [num] is a natural number or 'inf' for infinity")
+            print("Ctrl-D or Ctrl-C to exit")
+            exit(0)
+    else:
+        n = True
+
+if (n is None):
+    n = int(input("Введите количество примеров: "))
 
 programm_start_time = time.time()
 tasks_times = []
 right_answers = 0
-for i in range(n):
-    print(f"Вопрос {i+1}/{n}")
+i = 0
+while True:
+    i += 1
+    print(f"Вопрос {i}{f"/{n}" if n is int else ""}")
     a = randint(2, 9)
     b = randint(2, 9)
 
     start_time = time.time()
     answer = None
+    is_interrupted = False
     while True:
         try:
             answer = input(f"{a} * {b} = ")
             break
         except ValueError:
             print("Пожалуйста, введите целое число!")
+        except (KeyboardInterrupt, EOFError):
+            is_interrupted = True
+            i -= 1
+            break
+    if (is_interrupted): 
+        print()
+        break
     spend_time = time.time() - start_time
     if int(answer) == a*b:
         print(f"Верно! (Время: {spend_time:.1f} секунд)")
@@ -26,11 +51,15 @@ for i in range(n):
     else:
         print(f"Неверно! Правильно: {a*b} (Время: {spend_time:.1f} секунд)")
 
+    if (n is int and i == n):
+        break
+
+
 programm_spend_time = time.time() - programm_start_time
 print("="*30)
 print("СТАТИСТИКА:")
 print("="*30)
 print(f"Общее время: {programm_spend_time:.1f} секунд")
 print(f"Среднее время на вопрос: {programm_spend_time/n:.1f} секунд")
-print(f"Правильных ответов: {right_answers}/{n}")
-print(f"Процент пралвильных: {right_answers/n * 100:.1f}%")
+print(f"Правильных ответов: {right_answers}/{i}")
+print(f"Процент правильных: {right_answers/i * 100:.1f}%")
